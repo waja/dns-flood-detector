2e5faaf8b7c52cdbfa4700d3ab05d4a3f9d53914
Jan Wagner <waja@cyconet.org>
2006-11-09 21:38:39 +0000

    fix init script

--- debian/init.d
+++ debian/init.d
@@ -41,14 +41,14 @@ case "$1" in
 	echo -n "Stopping $DESC: "
 	start-stop-daemon --stop --quiet --pidfile /var/run/$NAME.pid \
 		--exec $DAEMON
-	killall $NAME
+	killall -9 $NAME
 	echo "$NAME."
 	;;
   restart|force-reload)
 	echo -n "Restarting $DESC: "
 	start-stop-daemon --stop --quiet --pidfile \
 		/var/run/$NAME.pid --exec $DAEMON
-	killall $NAME
+	killall -9 $NAME
 	sleep 1
 	start-stop-daemon --start --quiet --pidfile \
 		/var/run/$NAME.pid --exec $DAEMON -- $DAEMON_OPTS
